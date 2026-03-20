e6b8ac6a754cc22549a836dd6e66b99569e821e8
Ruslan <stadnikruslan168@gmail.com>
2026-03-20 12:16:28 +0200

    update settings & requirements.txt

--- university_site/settings.py
+++ university_site/settings.py
@@ -22,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-qb7!#%8tcqn*%)z=1l+ad$((_zb!os16@_$qeepf9emrqhnvxm')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
